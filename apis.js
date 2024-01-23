45fa82266b19c8b0afb30da3679b417a0aa8c9c0
Daniel Zeh <dfzeh1@gmail.com>
2024-01-23 09:59:54 -0600

    final commit

---
+++ apis.js
@@ -0,0 +1,40 @@
+//ANSWERS 
+// the height of Darth Vader
+// https://swapi.dev/api/people/4
+
+// the population of the planet Alderaan
+//https://swapi.dev/api/planets/2
+
+
+// the name of the manufacturer of the Millennium Falcon
+//https://swapi.dev/api/starships/10
+
+// the name of the species that C-3PO belongs to (multiple URLs)
+//https://swapi.dev/api/people/2
+//"https://swapi.dev/api/species/2/"
+
+// the title of each film that Obi-Wan Kenobi is in (multiple URLs)
+// "https://swapi.dev/api/films/1/",
+// "https://swapi.dev/api/films/2/",
+// "https://swapi.dev/api/films/3/",
+// "https://swapi.dev/api/films/4/",
+// "https://swapi.dev/api/films/5/",
+// "https://swapi.dev/api/films/6/"
+
+// use the search query (the how to on the search query is at the bottom of the Getting Started section of the documentation) to get the information about the Millennium Falcon, it’s a starship
+//https://swapi.dev/api/starships/?search=Millennium Falcon
+
+
+
+
+// The base URL of your requests is: https://practiceapi.devmountain.com/api (make sure to have the “s” in “https”)
+
+//Check if the POST request accept params, queries, and/or a body. Which one(s) and what information is it expecting to be sent?
+// body, JSON
+//What data type does the GET request return?
+//JSON body
+//What would the URL look like for deleting the post with the id 555? (This post does not exist anymore, but the syntax is the same for existing posts, )
+//https://practiceapi.devmountain.com/api/posts?id=1900
+//List the possible response codes from the GET request at ‘/posts/filter’
+//?text=red
+//Create a post whose text is your name, record the URL and body here:
